dd6d03682c9c905a736dfd1b666ce76176b19c28
Colton-2000 <colton.french@students.fhu.edu>
2024-02-01 17:28:07 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 #REMOVE PASS AND FIX THE FUNCTION
 def sum_of_products(list1, list2):
-    pass
+      list1 = list1.split()
+      list2 = list2.split()
+    if list1.len() == list2:
+        total = 0
+        for item in range(list1.len()):
+            mult = int(list1[item]) * int(list2[item])
+            total += mult
+        print(total)
+    else:
+        print('error')
 
 if __name__ == '__main__':
    #REMOVE PASS AND YOUR CODE GOES HERE
-    pass
+    inp1 = input()
+    inp = input()
+    sum_of_products(inp1, inp2)
